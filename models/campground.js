210904c6177029970378e39f74e2270d4f7bd2c4
Abhishekkd <ad7679@srmist.edu.in>
2021-12-01 23:22:51 +0530

    implemented mongoose middleware to delete associated reviews from db

--- models/campground.js
+++ models/campground.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 //to shorten mongoose.schema we're gonna save reference to  it in a variable
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 //our schema
 const CampgroundSchema = new Schema({
@@ -19,6 +20,29 @@ const CampgroundSchema = new Schema({
         }
     ]
 });
+//mongoose query middleware for deleting reviews along with their corresponding campground
+//post that is this will run after its been deleted
+CampgroundSchema.post('findOneAndDelete', async(doc)=>{
+    //doc the thing that was deleted and passed onto our middleware functions
+    //taking all of the reviews in the array called reviews inside campground ,take all
+    // the ids from that array and delete every review with that matching id 
+   
+    //if we did find a document i.e something is deleted
+    if(doc){
+        await Review.deleteMany({
+       //this document has review and we're going to basically delete all reviews where their id field in our document that
+       //was just deleted,in its reviews array (deleting from our reviews collection(based upon Review model(represents data in db))
+       // inside of our re-camp db) 
+       _id:{
+           $in: doc.reviews
+       }
+   })
+ 
+}
+  
+})
+
+
 
 //exporting our whole model without saving its reference to a variable
 module.exports = mongoose.model("Campground", CampgroundSchema);
