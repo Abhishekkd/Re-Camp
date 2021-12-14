4e26a0105c5edc4cb6a9ea0b697e170df63ee04b
Abhishekkd <ad7679@srmist.edu.in>
2021-12-15 00:22:06 +0530

    tweaking Cluster code

--- public/scripts/geoCluster.js
+++ public/scripts/geoCluster.js
@@ -1,3 +1,4 @@
+//campgrounds is our source because thats the dataset we are using
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 container: 'map',
@@ -11,7 +12,7 @@ map.on('load', () => {
 // Add a new source from our GeoJSON data and
 // set the 'cluster' option to true. GL-JS will
 // add the point_count property to your source data.
-map.addSource('earthquakes', {
+map.addSource('campgrounds', {//so here we are registering a source referring back to it later in layers
 type: 'geojson',
 // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
 // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
@@ -21,10 +22,10 @@ clusterMaxZoom: 14, // Max zoom to cluster points on
 clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
 });
  
-map.addLayer({
+map.addLayer({ //layer for circle
 id: 'clusters',
 type: 'circle',
-source: 'earthquakes',
+source: 'campgrounds',
 filter: ['has', 'point_count'],
 paint: {
 // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
@@ -35,12 +36,13 @@ paint: {
 'circle-color': [
 'step',
 ['get', 'point_count'],
-'#51bbd6',
+'#B91646',
 100,
 '#f1f075',
 750,
 '#f28cb1'
-],
+ //these controls the size and the shape of the circle depending upon the no of points in it so the number of things in a given cluster
+],  
 'circle-radius': [
 'step',
 ['get', 'point_count'],
@@ -54,9 +56,9 @@ paint: {
 });
  
 map.addLayer({
-id: 'cluster-count',
+id: 'cluster-count',//for the count of things in a cluster circle i.e text
 type: 'symbol',
-source: 'earthquakes',
+source: 'campgrounds',
 filter: ['has', 'point_count'],
 layout: {
 'text-field': '{point_count_abbreviated}',
@@ -64,17 +66,17 @@ layout: {
 'text-size': 12
 }
 });
- 
+//determine what the point should look like
 map.addLayer({
-id: 'unclustered-point',
+id: 'unclustered-point',//for a unclustered point i.e no cluster just a single point
 type: 'circle',
-source: 'earthquakes',
-filter: ['!', ['has', 'point_count']],
-paint: {
-'circle-color': '#11b4da',
+source: 'campgrounds',
+filter: ['!', ['has', 'point_count']], 
+paint: { //if we not "has" "point-count" then display circle of these configuration
+'circle-color': '#781D42',
 'circle-radius': 4,
 'circle-stroke-width': 1,
-'circle-stroke-color': '#fff'
+'circle-stroke-color': '#CD1818'
 }
 });
  
@@ -84,14 +86,14 @@ const features = map.queryRenderedFeatures(e.point, {
 layers: ['clusters']
 });
 const clusterId = features[0].properties.cluster_id;
-map.getSource('earthquakes').getClusterExpansionZoom(
+map.getSource('campgrounds').getClusterExpansionZoom(
 clusterId,
 (err, zoom) => {
 if (err) return;
  
 map.easeTo({
-center: features[0].geometry.coordinates,
-zoom: zoom
+center: features[0].geometry.coordinates,//whenever i click on something thats going to be the centre of zoom in
+zoom: zoom//zoom when we click on it
 });
 }
 );
